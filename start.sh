88133b41dfb69f7b70105b0462212f33fd944eed
Rohesh-Gavit <153485430+Rohesh-Gavit@users.noreply.github.com>
2023-12-13 11:02:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Rohesh-Gavit/-AwesomeFilterPro /EvaMaria
+  git clone https://github.com/Rohesh-Gavit/-AwesomeFilterPro / -AwesomeFilterPro
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO / -AwesomeFilterPro
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
